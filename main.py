270916485694f3beccd723d4b6101bd5aee16dd1
Wayzm1 <maxduka84@gmail.com>
2023-09-25 13:04:48 +0300

    1

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import*
+window = Tk()
+c = Canvas(window, width=100, height=100, bg='white')
+c.pack()
+c.create_line( 10,10,90,10)
+c.create_line( 50,20),(50,90)
+    fill='green',
+    with=3,
+    arrow=FIRST,
+    dash=(10,2),
+    activefill='red',
+    arrowshape=("10 25 10")
+window.mainloop()
